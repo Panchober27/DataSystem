d610da2066867b6d5304f9553a699fabf579294e
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2020-05-20 22:44:39 -0400

    Subida Inicial
    Subo la app en java modo escritorio, acuerdate de manterlas separadas ya que falta desarrollar esta wea

---
+++ README.txt
@@ -0,0 +1,5 @@
+Actualizacion.			01-05-2020 03:00hrs
+
+Terminados casi todos los diseños se respalda porque borrare netBeans 
+:)
+ESTE DOCUMENTO README CERTIFICA ESTA COMO LA PLANTILLA ORIGINAL DE LA APP.
---
+++ src/clases/Conexion.java
@@ -0,0 +1,18 @@
+package clases;
+
+import java.sql.*;
+
+public class Conexion {
+
+    public static Connection conectar() {
+        try {
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_ds", "root", "");
+            return cn;
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión local\n"
+                    + "error de tipo: " + e);
+        }
+        return null;
+    }
+
+}
